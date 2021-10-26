8ba43dee9c0f257b31286e8ce3cabef434ae8d0d
Jordan Neuman <jneum498@d127.org>
2021-10-26 14:05:29 -0500

    Initial Commit

---
+++ Display Number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Display Number
+//
+//  Created by Jordan S Neuman (Student) on 10/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
